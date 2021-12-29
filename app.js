807970560771f7a4e9b04dcbc8a028ba61f42daa
Dev Jadeja <devjadeja559@gmail.com>
2021-12-29 17:32:38 +0530

    added routes' structure

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const usersRoute = require("./routes/api/users");
+const authRoute = require("./routes/api/auth");
+const comparisonsRoute = require("./routes/api/comparisons");
 
 const app = express();
 connectDB();
 
 app.get("/", (req, res) => res.send("API running"));
+app.use("/api/users", usersRoute);
+app.use("/api/comparisons", authRoute);
+app.use("/api/auth", comparisonsRoute);
 
 const PORT = process.env.PORT || 5000;
 
